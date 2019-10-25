ab2379567d5ac0f83241422d0abc52610811a19d
daitianci <dtc6323833>
2019-10-25 16:33:51 +0800

    shiro init

--- src/main/java/io/renren/modules/sys/oauth2/OAuth2Token.java
+++ src/main/java/io/renren/modules/sys/oauth2/OAuth2Token.java
@@ -16,6 +16,8 @@ import org.apache.shiro.authc.AuthenticationToken;
  * 重写AuthenticationToken 使得 Principal 变成token
  * 1、OAuth2Realm 中 supports 判断token是否为自定义token
  * 2、OAuth2Realm 中 doGetAuthenticationInfo 中 根据传进来的uuid token获取当前登录用户
+ * 3、ShiroConfig 中 连接器链要配置 OAuth2Filter 拦截器
+ *    拦截器链增加oauth过滤,拦截所有请求，判断请求里是否有token，没有的话直接返回401，并且不继续处理，登录失败则返回错误信息，拦截到请求里有token信息，再进入登录验证
  * @author Mark sunlightcs@gmail.com
  */
 public class OAuth2Token implements AuthenticationToken {
